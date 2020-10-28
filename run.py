5bfabe920d59764131781592f09dc963a4d6257c
zahur76 <zahurmeerun@hotmail.com>
2020-10-28 13:50:50 +0000

    Code refactoring of codes

--- run.py
+++ run.py
@@ -1,45 +1,43 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
+
 
 app = Flask(__name__)
 app.secret_key = "randomstring123"
 messages = []
 
 
-def add_messages(username, message):
-    """Add messages to messages list"""
+def add_message(username, message):
+    """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
-    message_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(message_dict)
+    messages.append({"timestamp": now, "from": username, "message": message})
 
 
-@app.route("/", methods=["get", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-
     if request.method == "POST":
         session["username"] = request.form["username"]
 
     if "username" in session:
-        return redirect("/" + session["username"])
+        return redirect(url_for("user", username=session["username"]))
 
     return render_template("index.html")
 
 
-@app.route("/<username>")
-def name(username):
-    """Display chat message"""
-    return render_template("chat.html", username=username, chat_messages=messages)
+@app.route("/chat/<username>", methods=["GET", "POST"])
+def user(username):
+    """Add and display chat messages"""
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username, message)
+        return redirect(url_for("user", username=session["username"]))
+
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
 
 
-@app.route("/<username>/<message>")
-def send_message(username, message):
-    """Create a new message and redirect to chat message"""
-    add_messages(username, message)
-    return redirect("/" + username)
-
-
-app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-            debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
