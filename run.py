efb751f025af77f0f7c90384d37a3d4659c8219f
zahur76 <zahurmeerun@hotmail.com>
2020-10-28 05:17:26 +0000

    add message list

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 messages = []
 
 
 def add_messages(username, message):
+    """Add messages to messages list"""
     messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Format messages list"""
+    return "<br>".join(messages)
 
 
 @app.route("/")
@@ -18,13 +24,14 @@ def index():
 @app.route("/<username>")
 def name(username):
     """Display chat message"""
-    return "Welcome, {0}".format(username, messages)
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
     """Create a new message and redirect to chat message"""
-    return ("{0}: {1}").format(username, message)
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.environ.get('IP'),
