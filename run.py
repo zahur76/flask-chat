f8635362f9fb93674beeabacf5680cfa3ade91c9
zahur76 <zahurmeerun@hotmail.com>
2020-10-28 08:30:00 +0000

    Include index page and form

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 messages = []
+
 
 
 def add_messages(username, message):
@@ -17,10 +19,17 @@ def get_all_messages():
     return "<br>".join(messages)
 
 
-@app.route("/")
+@app.route("/", methods=["get", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use/USERNAME/MESSAGE"
+
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+
+    if "username" in session:
+        return redirect("/" + session["username"])
+
+    return render_template("index.html")
 
 
 @app.route("/<username>")
